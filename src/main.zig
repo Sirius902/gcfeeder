bd739a31c951eb305d7cda8858e21c1b693d1194
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2021-01-14 18:04:22 -0800

    Add random string display (testing)

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,11 @@ const print = std.debug.print;
 
 const c = @import("c.zig");
 
-pub fn main() void {
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = &gpa.allocator;
+
     var ctx: ?*c.libusb_context = null;
     _ = c.libusb_init(&ctx);
     defer c.libusb_exit(ctx);
@@ -17,15 +21,30 @@ pub fn main() void {
     const screen_height = 640;
 
     c.SetTraceLogLevel(c.LOG_NONE);
-    c.InitWindow(screen_width, screen_height, "raylib window");
+    c.InitWindow(screen_width, screen_height, "gcfeeder");
     c.SetTargetFPS(60);
+
+    var buffer = std.ArrayList(u8).init(allocator);
+    defer buffer.deinit();
+
+    // Add null-terminator.
+    try buffer.append(0);
+
+    var gen = std.rand.Xoroshiro128.init(@bitCast(u64, @truncate(i64, std.time.nanoTimestamp())));
+    const rand = &gen.random;
 
     while (!c.WindowShouldClose()) {
         c.BeginDrawing();
         defer c.EndDrawing();
 
-        c.ClearBackground(c.RAYWHITE);
-        c.DrawText("Congrats! You created your first window!", 190, 200, 20, c.LIGHTGRAY);
+        if (c.IsKeyDown(c.KEY_UP)) {
+            const letter = rand.intRangeLessThan(u8, 0, 26);
+            const base: u8 = if (rand.boolean()) 'a' else 'A';
+            try buffer.insert(buffer.items.len - 1, letter + base);
+        }
+
+        c.ClearBackground(c.DARKGRAY);
+        c.DrawText(buffer.items.ptr, 190, 200, 20, c.LIGHTGRAY);
     }
 
     c.CloseWindow();
