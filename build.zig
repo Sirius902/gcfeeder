328e5889d3801166d6ba46d3364bf798ee94f79b
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2021-01-19 19:15:58 -0800

    Fix build script

--- build.zig
+++ build.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const Builder = std.build.Builder;
+const Step = std.build.Step;
 
 pub fn build(b: *Builder) void {
     // Standard target options allows the person running `zig build` to choose
@@ -38,37 +39,50 @@ pub fn build(b: *Builder) void {
     exe.setBuildMode(mode);
     exe.install();
 
-    copyDlls(b, target_triple_str) catch |err| {
-        std.debug.warn("{} error while trying to copy dlls\n", .{err});
-        std.os.exit(1);
-    };
-
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
+
+    const dll_step = DllStep.create(b, target_triple_str);
+    run_cmd.step.dependOn(&dll_step.step);
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 }
 
-fn copyDlls(b: *Builder, target_triple_str: []const u8) !void {
-    var lib = try std.fs.cwd().openDir("lib", .{});
-    defer lib.close();
+const DllStep = struct {
+    step: Step,
+    builder: *Builder,
+    target_triple_str: []const u8,
 
-    var triple_lib = try lib.openDir(target_triple_str, .{ .iterate = true });
-    defer triple_lib.close();
+    pub fn create(b: *Builder, target_triple_str: []const u8) *DllStep {
+        var self = b.allocator.create(DllStep) catch unreachable;
+        self.* = DllStep{
+            .step = Step.init(.TopLevel, "dll", b.allocator, make),
+            .builder = b,
+            .target_triple_str = target_triple_str,
+        };
 
-    var exe_dir = try std.fs.cwd().openDir(b.exe_dir, .{});
-    defer exe_dir.close();
+        return self;
+    }
 
-    var files = triple_lib.iterate();
-    while (try files.next()) |file| {
-        const extension = fileExtension(file.name) orelse continue;
-        if (std.mem.eql(u8, extension, ".dll")) {
-            try triple_lib.copyFile(file.name, exe_dir, file.name, .{});
+    fn make(step: *Step) !void {
+        const self = @fieldParentPtr(DllStep, "step", step);
+        const b = self.builder;
+
+        var lib = try std.fs.cwd().openDir("lib", .{});
+        defer lib.close();
+
+        var triple_lib = try lib.openDir(self.target_triple_str, .{ .iterate = true });
+        defer triple_lib.close();
+
+        var exe_dir = try std.fs.cwd().openDir(b.exe_dir, .{});
+        defer exe_dir.close();
+
+        var files = triple_lib.iterate();
+        while (try files.next()) |file| {
+            if (std.mem.endsWith(u8, file.name, ".dll")) {
+                try triple_lib.copyFile(file.name, exe_dir, file.name, .{});
+            }
         }
     }
-}
-
-fn fileExtension(name: []const u8) ?[]const u8 {
-    return if (std.mem.lastIndexOf(u8, name, ".")) |i| name[i..] else null;
-}
+};
