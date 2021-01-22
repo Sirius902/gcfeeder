3b18d40c74f7b35519893c46eb3092d9d6d8f0f0
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2021-01-21 22:20:34 -0800

    Add Devices and DeviceList

--- src/usb.zig
+++ src/usb.zig
@@ -131,6 +131,56 @@ pub const Context = struct {
     }
 };
 
+pub const Devices = struct {
+    ctx: *Context,
+    devices: []?*c.libusb_device,
+    i: usize,
+
+    pub fn next(self: *Devices) ?Device {
+        if (self.i < self.devices.len) {
+            defer self.i += 1;
+            return Device.fromLibusb(self.ctx.ctx, self.devices[self.i].?);
+        } else {
+            return null;
+        }
+    }
+};
+
+pub const DeviceList = struct {
+    ctx: *Context,
+    list: [*c]?*c.libusb_device,
+    len: usize,
+
+    pub fn init(ctx: *Context) !DeviceList {
+        var list: [*c]?*c.libusb_device = undefined;
+        const n = c.libusb_get_device_list(ctx.ctx, &list);
+
+        if (n < 0) {
+            return errorFromLibusb(
+                std.math.cast(c_int, n) catch unreachable,
+            );
+        } else {
+            return DeviceList{
+                .ctx = ctx,
+                .list = list,
+                .len = std.math.cast(usize, n) catch unreachable,
+            };
+        }
+    }
+
+    pub fn deinit(self: DeviceList) void {
+        c.libusb_free_device_list(self.list, 1);
+    }
+
+    pub fn devices(self: DeviceList) Devices {
+        return Devices{
+            .ctx = self.ctx,
+            .devices = self.list[0..self.len],
+            .i = 0,
+        };
+    }
+};
+
 pub const Direction = enum {
     In,
     Out,
