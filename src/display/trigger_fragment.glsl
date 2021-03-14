48d8883c734e266b26137c7eb626efec866c8624
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2021-03-13 20:25:24 -0800

    Extract magic float constants and clean up a bit

--- src/display/trigger_fragment.glsl
+++ src/display/trigger_fragment.glsl
@@ -9,10 +9,14 @@ uniform float fill;
 
 const float radius = 0.5;
 
+const float threshold = 0.75;
+const float scale = 1.0 / threshold;
+
 void main() {
     float sq_dist = (radius * radius) - ((v_pos.x * v_pos.x) + (v_pos.y * v_pos.y));
 
-    if (sq_dist < 0.0 || ((sq_dist >= border_width * border_width) && (v_pos.x + 0.5 > clamp(fill, 0.0, 0.8) * (1.0 / 0.8)))) {
+    if (sq_dist < 0.0 || ((sq_dist >= border_width * border_width)
+         && (v_pos.x + 0.5 > clamp(fill, 0.0, threshold) * scale))) {
         discard;
     }
 
