f9079c4e90bfec358f0efc5de517d2a10720ed42
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2021-03-21 18:18:26 -0700

    Add aspect ratio correction

--- src/display/vertex.glsl
+++ src/display/vertex.glsl
@@ -6,11 +6,30 @@ out vec2 v_pos;
 out vec2 v_tex_coord;
 out float border_width;
 
+uniform vec2 window_size;
 uniform mat4 model;
 uniform float scale = 1.0;
 
 void main() {
-    gl_Position = model * vec4(a_pos, 0.0, 1.0);
+    float sx;
+    float sy;
+
+    if (window_size.x > window_size.y * 2.0) {
+        sx = (window_size.y * 2.0) / window_size.x;
+        sy = 2.0;
+    } else {
+        sx = 1.0;
+        sy = window_size.x / window_size.y;
+    }
+
+    mat4 aspect_matrix = mat4(
+        sx, 0.0, 0.0, 0.0,
+        0.0, sy, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0, 1.0
+    );
+
+    gl_Position = aspect_matrix * model * vec4(a_pos, 0.0, 1.0);
     v_pos = a_pos;
     v_tex_coord = a_tex_coord;
     border_width = 0.025 / scale;
