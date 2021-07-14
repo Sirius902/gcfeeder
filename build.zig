f43d2125aeee10cb361784d0d12528afcb705a75
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2021-07-14 00:19:06 -0700

    Try setting default target to x64 manually

--- build.zig
+++ build.zig
@@ -8,7 +8,9 @@ pub fn build(b: *Builder) void {
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
     // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
+    const target = b.standardTargetOptions(.{
+        .default_target = .{ .cpu_arch = .x86_64 },
+    });
 
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
