23bf7e85767f417738d7378e9a68abe7325bfcfe
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2021-07-14 14:09:56 -0700

    Specify default_target

--- build.zig
+++ build.zig
@@ -4,14 +4,10 @@ const Step = std.build.Step;
 const LibExeObjStep = std.build.LibExeObjStep;
 
 pub fn build(b: *Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
+    const target = b.standardTargetOptions(.{
+        .default_target = .{ .cpu_arch = .x86_64, .os_tag = .windows },
+    });
 
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
     const strip = b.option(bool, "strip", "Strip binary") orelse false;
 
