001b8b7d385b0aecfa0ad01989d1aa9a6a7a9774
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2022-09-11 12:49:23 -0700

    Add panic hook and set cwd to exe dir in main

--- src/main.rs
+++ src/main.rs
@@ -23,8 +23,18 @@ use log::warn;
 use simple_logger::SimpleLogger;
 use trayicon::{MenuBuilder, TrayIconBuilder};
 
-// TODO: Add a panic handler?, handle mutex poisoning.
+// TODO: Handle mutex poisoning.
 pub fn main() {
+    std::panic::set_hook(Box::new(panic::hook));
+
+    let exe_path = env::current_exe().expect("Failed to get current exe path");
+    env::set_current_dir(
+        exe_path
+            .parent()
+            .expect("Failed to get current exe parent path"),
+    )
+    .expect("Failed to set current working directory");
+
     SimpleLogger::new()
         .with_local_timestamps()
         .with_level(log::LevelFilter::Debug)
@@ -90,10 +100,7 @@ impl MyApp {
         ctrlc_reciever: channel::Receiver<()>,
         tray_reciever: channel::Receiver<TrayMessage>,
     ) -> Self {
-        let exe_path = env::current_exe().expect("Failed to get current exe path");
-        let mut config_path =
-            PathBuf::from(exe_path.parent().expect("Failed to get exe path parent"));
-        config_path.push(Self::CONFIG_PATH);
+        let config_path = Path::new(Self::CONFIG_PATH).to_path_buf();
 
         let config = Self::load_or_create_config(&config_path);
         let profile = {
@@ -271,5 +278,42 @@ impl eframe::App for MyApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             self.config_ui(ui);
         });
+    }
+}
+
+mod panic {
+    use std::{fs, io, panic::PanicInfo, thread};
+
+    use backtrace::Backtrace;
+
+    /// Attempts to log panic information with a backtrace to `panic.log` in
+    /// the current working directory.
+    ///
+    /// Implementation adapted from Rust's default panic hook.
+    pub fn hook(info: &PanicInfo<'_>) {
+        if let Ok(mut log) = fs::File::create("panic.log") {
+            // The current implementation always returns `Some`.
+            let location = info.location().unwrap();
+
+            let msg = match info.payload().downcast_ref::<&'static str>() {
+                Some(s) => *s,
+                None => match info.payload().downcast_ref::<String>() {
+                    Some(s) => &s[..],
+                    None => "Box<dyn Any>",
+                },
+            };
+            let thread = thread::current();
+            let name = thread.name().unwrap_or("<unnamed>");
+
+            let write = |err: &mut dyn io::Write| {
+                let _ = writeln!(err, "thread '{name}' panicked at '{msg}', {location}");
+
+                let backtrace = Backtrace::new();
+                let _ = writeln!(err, "stack backtrace:");
+                let _ = err.write_all(format!("{:#?}", backtrace).as_bytes());
+            };
+
+            write(&mut log);
+        }
     }
 }
