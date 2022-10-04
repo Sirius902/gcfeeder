d7597d6f29e24e021c2fea6d83c933da5f6a2c29
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2022-10-03 18:46:45 -0700

    Add overflow indicator for message count, only show one message for overflows

--- src/gui/app/panel/log.rs
+++ src/gui/app/panel/log.rs
@@ -3,7 +3,7 @@ use crossbeam::channel;
 
 pub struct LogPanel {
     receiver: channel::Receiver<Message>,
-    messages: Vec<(Message, usize)>,
+    messages: Vec<(Message, usize, bool)>,
     auto_scroll: bool,
 }
 
@@ -18,16 +18,19 @@ impl LogPanel {
 
     pub fn ui(&mut self, ui: &mut egui::Ui) {
         while let Ok(message) = self.receiver.try_recv() {
-            if let Some((last_message, last_count)) = self.messages.last_mut() {
+            if let Some((last_message, last_count, overflowed)) = self.messages.last_mut() {
                 if message == *last_message {
                     if let Some(count) = last_count.checked_add(1) {
                         *last_count = count;
                         continue;
+                    } else {
+                        *overflowed = true;
+                        break;
                     }
                 }
             }
 
-            self.messages.push((message, 1));
+            self.messages.push((message, 1, false));
         }
 
         ui.set_height(125.0);
@@ -53,11 +56,12 @@ impl LogPanel {
 
                 let grid = egui::Grid::new("messages").num_columns(3);
                 grid.show(ui, |ui| {
-                    for (message, count) in rows.map(|i| &self.messages[i]) {
+                    for (message, count, overflowed) in rows.map(|i| &self.messages[i]) {
                         ui.horizontal(|ui| {
                             message.draw(ui);
                             if *count != 1 {
-                                ui.label(format!("(x{})", *count));
+                                let symbol = if *overflowed { ">" } else { "x" };
+                                ui.label(format!("({}{})", symbol, *count));
                             }
                         });
 
