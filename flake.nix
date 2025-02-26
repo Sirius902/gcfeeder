5b6fb8748466989f6ee68cbd4ffe370bb4004121
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-02-25 19:40:52 -0800

    feat: add justfile

--- flake.nix
+++ flake.nix
@@ -188,6 +188,7 @@
               pkgs.lldb
               pkgs.rust-analyzer-nightly
               pkgs.taplo-cli
+              pkgs.just
             ];
 
             LD_LIBRARY_PATH = lib.makeLibraryPath commonArgs.buildInputs;
---
+++ justfile
@@ -0,0 +1,3 @@
+fmt:
+  @cargo fmt
+  @taplo fmt
