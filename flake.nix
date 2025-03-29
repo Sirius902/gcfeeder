2849725a40ea7e382c39dce064dc3088eb97b1b1
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-03-29 15:39:57 -0700

    chore(deps): cargo update
    chore(nix): nix flake update

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
 
         toolchain = fenix.packages.${system}.fromToolchainFile {
           file = ./rust-toolchain.toml;
-          sha256 = "sha256-Zg9aFWJDxc6US8d/da7pnq7QTG05J5MjjMmGv2sfGeM=";
+          sha256 = "sha256-zA9ZxrIQn1RzSn83jL64z4UtnLTY2d1jRI/IfORevLk=";
         };
 
         craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
