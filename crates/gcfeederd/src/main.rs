46c9ad240f9f1f812e98796deb814194656a1019
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-03-29 17:27:01 -0700

    feat: log to file

--- crates/gcfeederd/src/main.rs
+++ crates/gcfeederd/src/main.rs
@@ -9,7 +9,7 @@ use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() -> adapter::Result<()> {
-    init_logging();
+    let _guard = setup_logging();
 
     let task_tracker = TaskTracker::new();
 
@@ -36,7 +36,7 @@ async fn main() -> adapter::Result<()> {
     Ok(())
 }
 
-fn init_logging() {
+fn setup_logging() -> Option<tracing_appender::non_blocking::WorkerGuard> {
     let builder = tracing_subscriber::registry();
 
     #[cfg(feature = "tokio-console")]
@@ -49,13 +49,34 @@ fn init_logging() {
             .expect("tokio-console env filter string parses")
     }));
 
-    builder
-        .with(tracing_subscriber::fmt::layer().with_filter(
-            EnvFilter::try_from_default_env().unwrap_or_else(|_| {
-                EnvFilter::builder()
-                    .parse(["gcfeederd=trace", "gcfeeder_core=trace"].join(","))
-                    .expect("env filter string parses")
-            }),
-        ))
-        .init();
+    let env_filter = || {
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+            EnvFilter::builder()
+                .parse(["gcfeederd=trace", "gcfeeder_core=trace"].join(","))
+                .expect("env filter string parses")
+        })
+    };
+
+    let builder = builder.with(tracing_subscriber::fmt::layer().with_filter(env_filter()));
+
+    let file_layer = directories::BaseDirs::new()
+        .map(|dirs| dirs.cache_dir().join("gcfeederd").join("logs"))
+        .map(|logs_dir| {
+            let file_appender = tracing_appender::rolling::daily(logs_dir, "gcfeederd.log");
+            let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
+            (
+                tracing_subscriber::fmt::layer()
+                    .with_writer(non_blocking)
+                    .with_filter(env_filter()),
+                guard,
+            )
+        });
+
+    if let Some((file_layer, guard)) = file_layer {
+        builder.with(file_layer).init();
+        Some(guard)
+    } else {
+        builder.init();
+        None
+    }
 }
